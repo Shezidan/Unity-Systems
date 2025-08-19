f64901504211b3dff52e888f4413982f5bf8829c
Shezidan <149416008+Shezidan@users.noreply.github.com>
2025-08-19 02:29:08 +0100

    Created a GetPath method.

--- SaveSystem.cs
+++ SaveSystem.cs
@@ -5,18 +5,22 @@ public static class SaveSystem
 {
     public static void Save<T>(T saveData) where T : ISaveData
     {
-        string fileName = typeof(T).Name + ".json";
-        string path = Path.Combine(Application.persistentDataPath, fileName);
+        string path = GetPath<T>();
         var data = JsonUtility.ToJson(saveData, true);
         File.WriteAllText(path, data);
     }
 
     public static T Load<T>()
     {
-        string fileName = typeof(T).Name + ".json";
-        string path = Path.Combine(Application.persistentDataPath, fileName);
+        string path = GetPath<T>();
         string json = File.ReadAllText(path);
         return JsonUtility.FromJson<T>(json);
+    }
+
+    public static string GetPath<T>()
+    {
+        string fileName = typeof(T).Name + ".json";
+        return Path.Combine(Application.persistentDataPath, fileName); 
     }
 }
 
